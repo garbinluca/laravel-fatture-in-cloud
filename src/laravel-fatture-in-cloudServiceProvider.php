b882903c1f0c6155237f9bb0bf167009388975ab
Luca Garbin <hello@lucagarbin.it>
2018-01-12 18:12:55 +0100

    first commit

---
+++ src/laravel-fatture-in-cloudServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace garbos\laravel-fatture-in-cloud;
+
+use Illuminate\Support\ServiceProvider;
+
+class laravel-fatture-in-cloudServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
